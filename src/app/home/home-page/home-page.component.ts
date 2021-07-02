c2a0187a966beae252d9a6e75dfe439184946fe9
sandeep4102 <sandeep.sarswat@mail.vinove.com>
2021-07-02 11:52:11 +0530

    final changes

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,38 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
-
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home-page',
-  templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.css"],
 })
 export class HomePageComponent implements OnInit {
-  myimage:string="../../../assets/images/Stylo Design Institute.gif";
-  constructor(private router : Router) { }
+  myimage: string = "../../../assets/images/Stylo Design Institute.gif";
+  constructor(private router: Router) {}
 
-  userLogin()
-  {
-    this.router.navigateByUrl('/login');
+  userLogin() {
+    this.router.navigateByUrl("/login");
   }
-  // adminLogin()
-  // {
-  //   this.router.navigateByUrl('/adminLogin'); 
-  // }
 
   ngOnInit(): void {
-    this.checkAuth()
+    this.checkAuth();
   }
 
   checkAuth() {
-    if (localStorage.getItem('userDetails')) {
-      localStorage.removeItem('userDetails');
+    if (localStorage.getItem("userDetails")) {
+      localStorage.removeItem("userDetails");
     }
-    if(localStorage.getItem('adminDetail'))
-    {
-      localStorage.removeItem('adminDetail');
+    if (localStorage.getItem("adminDetail")) {
+      localStorage.removeItem("adminDetail");
     }
   }
-
 }
--- src/app/user/user-login/user-login.component.ts
+++ src/app/user/user-login/user-login.component.ts
@@ -10,9 +10,8 @@ import { GetDetailsService } from '../../services/getDetailService/get-details.s
   styleUrls: ['./user-login.component.css'],
 })
 export class UserLoginComponent implements OnInit {
-  public email: any;
-  public password: any;
-  public userCredentials: any;
+  public email: string;
+  public password: string;
   public flag = false;
   public showLoader: boolean = false;
   constructor(
@@ -52,22 +51,7 @@ export class UserLoginComponent implements OnInit {
       }
     );
   }
-  // userAuth(){
-  //   this.service.userData().subscribe(data=>{
-  //     this.userCredentials=data;
-  //     for(let i=0;i<this.userCredentials.length;i++){
-  //       if(this.userCredentials[i].email==this.email && this.userCredentials[i].password==this.password && this.userCredentials[i].block==false){
-  //         localStorage.setItem('userEmail', 'authenticate')
-  //         this.service.send.next(this.userCredentials[i]);
-  //         this.router.navigateByUrl('userDashboard');
-  //       }
-  //       else {
-  //         this.flag=true;
-  //       }
-  //     }
-  //   })
-  // }
-
+ 
   userAuth() {
     if (this.email && this.password) {
       this.allUserDetails.filter((val) => {
